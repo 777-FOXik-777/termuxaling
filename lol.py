40ad15e841a4c463c094739c78f0f202b4c3df1d
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-04 09:11:20 +0300

    Update lol.py

--- lol.py
+++ lol.py
@@ -12,14 +12,21 @@ print('Welcome to Termux!')
 print('\n')
 print('Community forum: https://termux.com/community')
 print('Gitter chat:     https://gitter.im/termux/termux')
-
-
-
-
-
-
-
-
+print('IRC channel:     #termux on libera.chat')
+print('\n')
+print('Working with packages:')
+print('\n')
+print(' * Search packages:   pkg search <query>')
+print(' * Install a package: pkg install <package>')
+print(' * Upgrade packages:  pkg upgrade')
+print('\n')
+print('Subscribing to additional repositories:')
+print('\n')
+print(' * Root:     pkg install root-repo')
+print(' * X11:      pkg install x11-repo')
+print('\n')
+print('Report issues at https://termux.com/issues')
+print('\n')
 while True:
     tsu = input('$ ')
     if tsu == '':
